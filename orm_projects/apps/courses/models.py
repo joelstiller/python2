a5553f0f342c3877250f7aa21b54f7b9d61ad802
Kassimila <joelstiller@gmail.com>
2018-02-24 22:05:48 -0600

    Working on courses

---
+++ orm_projects/apps/courses/models.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.db import models
+
+class CourseManager(models.Manager):
+    def validate(self, post_data):
+        errors = {}
+
+        # check all fields for emptyness
+        for field, value in post_data.iteritems():
+            if len(value) < 1:
+                errors[field] = "{} field is reqired".format(field.replace('_', ' '))
+
+            # check name fields for min length
+            if field == "course_name":
+                if not field in errors and len(value) < 5:
+                    errors[field] = "{} field must bet at least 5 characters".format(field.replace('_', ' '))
+            if field == "description":
+                if not field in errors and len(value) < 15:
+                    errors[field] = "{} field must bet at least 5 characters".format(field.replace('_', ' '))               
+        return errors
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = CourseManager()
