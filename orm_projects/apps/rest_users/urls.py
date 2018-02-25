645c282dd9dbb0019f7ab7e6de73da25620dfa3a
Kassimila <joelstiller@gmail.com>
2018-02-24 20:47:40 -0600

    Completed rest_users

--- orm_projects/apps/rest_users/urls.py
+++ orm_projects/apps/rest_users/urls.py
@@ -3,9 +3,11 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    # url(r'^new$', views.new),
-    # url(r'^create$', views.create),
-    # url(r'^(?P<user_id>\d+)$', views.show),
-    # url(r'^(?P<user_id>\d+)/edit$', views.edit),
-    # url(r'^(?P<user_id>\d+)/delete$', views.delete)
+    url(r'^users$', views.index),
+    url(r'^users/new$', views.new),
+    url(r'^users/create$', views.create),
+    url(r'^users/(?P<user_id>\d+)$', views.show),
+    url(r'^users/(?P<user_id>\d+)/edit$', views.edit),
+    url(r'^users/(?P<user_id>\d+)/update$', views.update),
+    url(r'^users/(?P<user_id>\d+)/destroy$', views.destroy)
 ]
